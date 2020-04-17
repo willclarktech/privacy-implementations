3814c120b2f7414e01cb19b87b10d98d9940bfdd
willclarktech <willclarktech@users.noreply.github.com>
2020-04-17 15:07:00 +0100

    :bug: Improve paillier multiplication by 0 and 1

--- src/paillier/homomorphic-math.ts
+++ src/paillier/homomorphic-math.ts
@@ -1,11 +1,23 @@
 import { modPow } from "bigint-crypto-utils";
 
+import { encrypt } from "./encryption";
 import { PublicKey } from "./keys";
 
 export const add = ({ n2 }: PublicKey) => (a: bigint, b: bigint): bigint =>
 	(a * b) % n2;
 
-export const multiply = ({ n2 }: PublicKey) => (
+export const multiply = (publicKey: PublicKey) => (
 	cipherText: bigint,
 	plainText: bigint,
-): bigint => modPow(cipherText, plainText, n2);
+): bigint => {
+	// Insecure naive multiplication: cipherText^0 % n^2 === 1
+	if (plainText === 0n) {
+		return encrypt(publicKey)(0n);
+	}
+	// Insecure naive multiplication: cipherText^1 % n^2 === cipherText
+	if (plainText === 1n) {
+		const encryptedZero = encrypt(publicKey)(0n);
+		return add(publicKey)(cipherText, encryptedZero);
+	}
+	return modPow(cipherText, plainText, publicKey.n2);
+};
