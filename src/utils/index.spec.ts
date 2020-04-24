f88891a790d18161879a3041a4189c082047e834
willclarktech <willclarktech@users.noreply.github.com>
2020-04-24 15:22:53 +0100

    :seedling: Add more utils

--- src/utils/index.spec.ts
+++ src/utils/index.spec.ts
@@ -1,4 +1,4 @@
-import { getBitLength } from "./";
+import { bigInt2Buffer, buffer2BigInt, getBitLength, getByteLength } from "./";
 
 describe("getBitLength", () => {
 	it.each([
@@ -11,6 +11,69 @@ describe("getBitLength", () => {
 		[9007199254740992n, 54],
 	])("calculates bit length for %d = %d", (a, expected) => {
 		const actual = getBitLength(a);
+		expect(actual).toStrictEqual(expected);
+	});
+});
+
+describe("getByteLength", () => {
+	it.each([
+		[0n, 1],
+		[1n, 1],
+		[2n, 1],
+		[16n, 1],
+		[255n, 1],
+		[256n, 2],
+		[65535n, 2],
+		[65536n, 3],
+		[9007199254740992n, 7],
+	])("calculates byte length for %d = %d", (a, expected) => {
+		const actual = getByteLength(a);
+		expect(actual).toStrictEqual(expected);
+	});
+});
+
+describe("bigInt2Buffer", () => {
+	it.each([
+		[0n, Buffer.alloc(8, 0x00)],
+		[1n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x01])])],
+		[2n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x02])])],
+		[16n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x10])])],
+		[255n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0xff])])],
+		[256n, Buffer.concat([Buffer.alloc(6, 0x00), Buffer.from([0x01, 0x00])])],
+		[65535n, Buffer.concat([Buffer.alloc(6, 0x00), Buffer.from([0xff, 0xff])])],
+		[
+			65536n,
+			Buffer.concat([Buffer.alloc(5, 0x00), Buffer.from([0x01, 0x00, 0x00])]),
+		],
+		[
+			9007199254740992n,
+			Buffer.from([0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
+		],
+	])("creates a buffer from %d = %o", (a, expected) => {
+		const actual = bigInt2Buffer(a);
+		expect(actual.equals(expected)).toStrictEqual(true);
+	});
+});
+
+describe("buffer2BigInt", () => {
+	it.each([
+		[Buffer.alloc(8, 0x00), 0n],
+		[Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x01])]), 1n],
+		[Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x02])]), 2n],
+		[Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x10])]), 16n],
+		[Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0xff])]), 255n],
+		[Buffer.concat([Buffer.alloc(6, 0x00), Buffer.from([0x01, 0x00])]), 256n],
+		[Buffer.concat([Buffer.alloc(6, 0x00), Buffer.from([0xff, 0xff])]), 65535n],
+		[
+			Buffer.concat([Buffer.alloc(5, 0x00), Buffer.from([0x01, 0x00, 0x00])]),
+			65536n,
+		],
+		[
+			Buffer.from([0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
+			9007199254740992n,
+		],
+	])("creates a bigint from %o = %d", (a, expected) => {
+		const actual = buffer2BigInt(a);
 		expect(actual).toStrictEqual(expected);
 	});
 });
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,16 @@
 export const getBitLength = (a: bigint): number =>
 	[0n, 1n].includes(a) ? 1 : 1 + getBitLength(a >> 1n);
+
+export const getByteLength = (a: bigint): number =>
+	Math.ceil(getBitLength(a) / 8);
+
+const MIN_BUFFER_SIZE = 8;
+
+export const bigInt2Buffer = (a: bigint): Buffer => {
+	const byteLength = Math.max(MIN_BUFFER_SIZE, getByteLength(a) + 1);
+	const buffer = Buffer.alloc(byteLength);
+	buffer.writeBigUInt64BE(a);
+	return buffer;
+};
+
+export const buffer2BigInt = (a: Buffer): bigint => a.readBigUInt64BE();
