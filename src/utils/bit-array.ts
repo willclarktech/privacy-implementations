d1d7c4dc32964e897527ae64c1ba5998b99fd7d9
willclarktech <willclarktech@users.noreply.github.com>
2020-04-27 16:06:15 +0100

    :recycle: Improve bit array implementation

--- src/utils/bit-array.ts
+++ src/utils/bit-array.ts
@@ -1,20 +1,25 @@
 export class BitArray {
-	public readonly bitLength: number;
+	public readonly bitLength: number | null;
 	private bits: bigint;
 
-	constructor(bitLength: number) {
-		this.bitLength = bitLength;
+	constructor(bitLength?: number) {
 		this.bits = 0n;
+		this.bitLength = bitLength === undefined ? null : bitLength;
 	}
 
 	public set(i: number): void {
-		if (i < 0 || i >= this.bitLength) {
-			throw new Error("Bit index out of range");
-		}
+		this.validateIndex(i);
 		this.bits |= 1n << BigInt(i);
 	}
 
 	public has(i: number): boolean {
+		this.validateIndex(i);
 		return !!(this.bits & (1n << BigInt(i)));
+	}
+
+	private validateIndex(i: number): void {
+		if (i < 0 || (this.bitLength !== null && i >= this.bitLength)) {
+			throw new Error("Bit index out of range");
+		}
 	}
 }
