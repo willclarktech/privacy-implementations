7fe8c092fa0fe7a57f4df0fb63af87faa331f6cd
willclarktech <willclarktech@users.noreply.github.com>
2020-05-13 14:36:28 +0100

    :seedling: Improve buffer <-> bigint conversion utils

--- src/util/index.ts
+++ src/util/index.ts
@@ -4,13 +4,14 @@ export const getBitLength = (a: bigint): number =>
 export const getByteLength = (a: bigint): number =>
 	Math.ceil(getBitLength(a) / 8);
 
-const MIN_BUFFER_SIZE = 8;
+export const bigInt2Buffer = (a: bigint): Buffer =>
+	Buffer.from(a.toString(16), "hex");
 
-export const bigInt2Buffer = (a: bigint): Buffer => {
-	const byteLength = Math.max(MIN_BUFFER_SIZE, getByteLength(a) + 1);
-	const buffer = Buffer.alloc(byteLength);
-	buffer.writeBigUInt64BE(a);
-	return buffer;
-};
+export const bigInt2Array = (a: bigint): readonly number[] =>
+	Array.from(bigInt2Buffer(a));
 
-export const buffer2BigInt = (a: Buffer): bigint => a.readBigUInt64BE();
+export const buffer2BigInt = (a: Buffer): bigint =>
+	BigInt(`0x${a.toString("hex")}`);
+
+export const array2BigInt = (a: readonly number[]): bigint =>
+	buffer2BigInt(Buffer.from(a));
