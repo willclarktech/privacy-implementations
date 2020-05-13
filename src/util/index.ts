5a75103cf613ad291db647ea2855807eaa5110c6
willclarktech <willclarktech@users.noreply.github.com>
2020-05-13 15:38:44 +0100

    :recycle: Move DH PSI utils into general util module

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,6 @@
+import { gcd, modPow } from "bigint-crypto-utils";
+import { createHash } from "crypto";
+
 export const getBitLength = (a: bigint): number =>
 	[0n, 1n].includes(a) ? 1 : 1 + getBitLength(a >> 1n);
 
@@ -15,3 +18,79 @@ export const buffer2BigInt = (a: Buffer): bigint =>
 
 export const array2BigInt = (a: readonly number[]): bigint =>
 	buffer2BigInt(Buffer.from(a));
+
+export const hashNumber = (algorithm: string, n: bigint): bigint => {
+	const buffer = Buffer.alloc(8);
+	buffer.writeBigUInt64BE(n);
+
+	const hash = createHash(algorithm);
+	hash.update(buffer);
+	const hashOutput = hash.digest();
+
+	return hashOutput.readBigUInt64BE();
+};
+
+export const phi = (n: bigint): bigint => {
+	let count = 0n;
+
+	for (let candidate = 1n; candidate < n; ++candidate) {
+		if (gcd(n, candidate) === 1n) {
+			++count;
+		}
+	}
+
+	return count;
+};
+
+const newtonIteration = (n: bigint, candidate: bigint): bigint => {
+	const nextCandidate = (n / candidate + candidate) >> 1n;
+	return candidate === nextCandidate || candidate === nextCandidate - 1n
+		? candidate + 1n
+		: newtonIteration(n, nextCandidate);
+};
+
+const getRoughSquareRoot = (n: bigint): bigint => {
+	if (n < 0n) {
+		throw new Error("Cannot get square root of negative numbers");
+	}
+	if (n < 2n) {
+		return n;
+	}
+	return newtonIteration(n, 1n);
+};
+
+export const getPrimeFactors = (n: bigint): readonly bigint[] => {
+	const roughSquareRoot = getRoughSquareRoot(n);
+	const primeFactors = [];
+
+	for (let candidate = 2n; candidate <= roughSquareRoot; ++candidate) {
+		if (n % candidate === 0n) {
+			primeFactors.push(candidate);
+		}
+	}
+
+	return primeFactors;
+};
+
+export const isGenerator = (
+	modulus: bigint,
+	candidate: bigint,
+	totient = phi(modulus),
+): boolean => {
+	const roughSquareRoot = getRoughSquareRoot(totient);
+
+	for (
+		let factorCandidate = 2n;
+		factorCandidate <= roughSquareRoot;
+		++factorCandidate
+	) {
+		if (
+			totient % factorCandidate === 0n &&
+			modPow(candidate, totient / factorCandidate, modulus) === 1n
+		) {
+			return false;
+		}
+	}
+
+	return true;
+};
