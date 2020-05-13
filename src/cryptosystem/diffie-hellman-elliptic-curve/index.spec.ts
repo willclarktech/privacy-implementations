544dbad8b3d97b58d6645138bb77eef631b7cce3
willclarktech <willclarktech@users.noreply.github.com>
2020-05-13 14:37:06 +0100

    :seedling: Add ECDH cryptosystem

---
+++ src/cryptosystem/diffie-hellman-elliptic-curve/index.spec.ts
@@ -0,0 +1,29 @@
+import { Party } from "./";
+
+describe("Elliptic Curve Diffie-Hellman", () => {
+	it("successfully completes key exchange", () => {
+		const alice = new Party();
+		const bob = new Party();
+
+		const aliceSharedSecret = alice.raise(bob.intermediateValue);
+		const bobSharedSecret = bob.raise(alice.intermediateValue);
+
+		expect(bobSharedSecret).toStrictEqual(aliceSharedSecret);
+		expect(aliceSharedSecret).not.toBeNull();
+		expect(aliceSharedSecret).not.toBeNaN();
+		expect(aliceSharedSecret).toBeGreaterThanOrEqual(1);
+	});
+
+	it("successfully inverts a raise", () => {
+		const alice = new Party();
+		const bob = new Party();
+
+		const plainText = 123n;
+		const raisedByAlice = alice.raise(plainText, true);
+		const raisedByBob = bob.raise(plainText, true);
+		const raisedByBoth = bob.raise(raisedByAlice);
+		const invertedByAlice = alice.raiseInverse(raisedByBoth);
+
+		expect(invertedByAlice).toStrictEqual(raisedByBob);
+	});
+});
---
+++ src/cryptosystem/diffie-hellman-elliptic-curve/index.ts
@@ -0,0 +1,56 @@
+import { modInv } from "bigint-crypto-utils";
+import BN from "bn.js";
+import { curve, ec as EllipticCurve } from "elliptic";
+
+import { array2BigInt, bigInt2Array, bigInt2Buffer } from "../../util";
+
+export type PartyOptions = {};
+
+export class Party {
+	private readonly ec: EllipticCurve;
+	private readonly curve: curve.short;
+	private readonly keyPair: EllipticCurve.KeyPair;
+	private readonly privateKeyInverse: BN;
+
+	constructor(_options: PartyOptions = {}) {
+		this.ec = new EllipticCurve("p256");
+		this.curve = this.ec.curve as curve.short;
+		this.keyPair = this.ec.genKeyPair();
+		this.privateKeyInverse = new BN(
+			bigInt2Buffer(
+				modInv(BigInt(this.keyPair.getPrivate()), BigInt(this.curve.n)),
+			),
+		);
+	}
+
+	public raise(g: bigint, shouldHash = false): bigint {
+		const point = shouldHash
+			? this.hashToPoint(bigInt2Array(g))
+			: this.curve.pointFromX(bigInt2Array(g));
+		const multiplicationPoint = point.mul(this.keyPair.getPrivate());
+		return BigInt(multiplicationPoint.getX());
+	}
+
+	public raiseInverse(g: bigint): bigint {
+		const point = this.curve.pointFromX(bigInt2Array(g));
+		const multiplicationPoint = point.mul(this.privateKeyInverse);
+		return BigInt(multiplicationPoint.getX());
+	}
+
+	public get intermediateValue(): bigint {
+		return this.raise(BigInt(this.ec.g.x), true);
+	}
+
+	private hashToPoint(input: readonly number[]): curve.short.ShortPoint {
+		const hashOutput = this.ec.hash().update(input).digest();
+		const x = array2BigInt(hashOutput) % BigInt(this.curve.p);
+		try {
+			return this.curve.pointFromX(bigInt2Array(x));
+		} catch (error) {
+			if (/invalid point/i.test(error)) {
+				return this.hashToPoint(bigInt2Array(x));
+			}
+			throw error;
+		}
+	}
+}
