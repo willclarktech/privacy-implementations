bfbf6293e5901ec6c875cb8b6a83a8cf42c80d7a
willclarktech <willclarktech@users.noreply.github.com>
2020-05-20 12:36:27 +0100

    :white_check_mark: Fix bigInt2Buffer test

--- src/util/index.spec.ts
+++ src/util/index.spec.ts
@@ -34,20 +34,17 @@ describe("getByteLength", () => {
 
 describe("bigInt2Buffer", () => {
 	it.each([
-		[0n, Buffer.alloc(8, 0x00)],
-		[1n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x01])])],
-		[2n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x02])])],
-		[16n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0x10])])],
-		[255n, Buffer.concat([Buffer.alloc(7, 0x00), Buffer.from([0xff])])],
-		[256n, Buffer.concat([Buffer.alloc(6, 0x00), Buffer.from([0x01, 0x00])])],
-		[65535n, Buffer.concat([Buffer.alloc(6, 0x00), Buffer.from([0xff, 0xff])])],
-		[
-			65536n,
-			Buffer.concat([Buffer.alloc(5, 0x00), Buffer.from([0x01, 0x00, 0x00])]),
-		],
+		[0n, Buffer.from([0x00])],
+		[1n, Buffer.from([0x01])],
+		[2n, Buffer.from([0x02])],
+		[16n, Buffer.from([0x10])],
+		[255n, Buffer.from([0xff])],
+		[256n, Buffer.from([0x01, 0x00])],
+		[65535n, Buffer.from([0xff, 0xff])],
+		[65536n, Buffer.from([0x01, 0x00, 0x00])],
 		[
 			9007199254740992n,
-			Buffer.from([0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
+			Buffer.from([0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
 		],
 	])("creates a buffer from %d = %o", (a, expected) => {
 		const actual = bigInt2Buffer(a);
