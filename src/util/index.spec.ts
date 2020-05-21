6d155c07ae9e4145390f3d941891f26aba192795
willclarktech <willclarktech@users.noreply.github.com>
2020-05-21 16:26:55 +0100

    :fire: Remove unused utils

--- src/util/index.spec.ts
+++ src/util/index.spec.ts
@@ -1,36 +1,4 @@
-import { bigInt2Buffer, buffer2BigInt, getBitLength, getByteLength } from "./";
-
-describe("getBitLength", () => {
-	it.each([
-		[0n, 1],
-		[1n, 1],
-		[2n, 2],
-		[3n, 2],
-		[15n, 4],
-		[16n, 5],
-		[9007199254740992n, 54],
-	])("calculates bit length for %d = %d", (a, expected) => {
-		const actual = getBitLength(a);
-		expect(actual).toStrictEqual(expected);
-	});
-});
-
-describe("getByteLength", () => {
-	it.each([
-		[0n, 1],
-		[1n, 1],
-		[2n, 1],
-		[16n, 1],
-		[255n, 1],
-		[256n, 2],
-		[65535n, 2],
-		[65536n, 3],
-		[9007199254740992n, 7],
-	])("calculates byte length for %d = %d", (a, expected) => {
-		const actual = getByteLength(a);
-		expect(actual).toStrictEqual(expected);
-	});
-});
+import { bigInt2Buffer, buffer2BigInt } from "./";
 
 describe("bigInt2Buffer", () => {
 	it.each([
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,9 +1,3 @@
-export const getBitLength = (a: bigint): number =>
-	[0n, 1n].includes(a) ? 1 : 1 + getBitLength(a >> 1n);
-
-export const getByteLength = (a: bigint): number =>
-	Math.ceil(getBitLength(a) / 8);
-
 export const bigInt2Buffer = (a: bigint, numBytes?: number): Buffer => {
 	const hex = a.toString(16);
 	const buffer = Buffer.from(hex.length % 2 ? `0${hex}` : hex, "hex");
@@ -22,6 +16,3 @@ export const bigInt2Array = (a: bigint): readonly number[] =>
 
 export const buffer2BigInt = (a: Buffer): bigint =>
 	BigInt(`0x${a.toString("hex")}`);
-
-export const array2BigInt = (a: readonly number[]): bigint =>
-	buffer2BigInt(Buffer.from(a));
